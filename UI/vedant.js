e2eac828368262568bf8f2c18a2ef93713d282de
Vedant Agrawal <70770638+veda424@users.noreply.github.com>
2024-09-04 17:38:01 +0530

    Update vedant.js

--- UI/vedant.js
+++ UI/vedant.js
@@ -35,7 +35,7 @@ botOptions.ttsSocketUrl = ''; // This option is deprecated
 botOptions.assertionFn = assertion;
 botOptions.koreAnonymousFn = koreAnonymousFn;
 botOptions.botInfo = {"name":"XYZ usecase", "_id" :"st-938cd739-71eb-5df4-a709-05d7913c9a01"};  //Capture Bot Name & Bot ID from Builder Tool app. Go to respective Bot and then navigate to Settings-->Config Settings-->General settings section. Bot Name is case sensitive.
-botOptions.JWTUrl ="https://jwt.io/#debugger-io?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhZ3Jhd2FsLnZlZGFudEB0Y3MuY29tIiwiaXNzdWVyIjoiY3MtNmE2OWNmZGUtMTlhOS01Y2IzLTlmYTMtMDgxMjRkODEyNGExIiwiaWF0IjoxNTE2MjM5MDIyfQ.ZOHoydxc3PFBF-v47jOCM7OmUz6AsqKO9UsVm2Q3Kk4";//above assertion function  picks url from here
+botOptions.JWTUrl ="https://jwt.io/#debugger-io?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWJqZWN0IjoiYWdyYXdhbC52ZWRhbnRAdGNzLmNvbSIsImlzc3VlciI6ImNzLTZhNjljZmRlLTE5YTktNWNiMy05ZmEzLTA4MTI0ZDgxMjRhMSIsImlhdCI6MTUxNjIzOTAyMn0.qF5-l4pGHe2cErkjgb7aWRVsqYasSoOOh6UjV4gHGYQ";//above assertion function  picks url from here
 botOptions.userIdentity = 'agrawal.vedant@tcs.com';// Provide users email id here
 botOptions.clientId   = "cs-6a69cfde-19a9-5cb3-9fa3-08124d8124a1"; // issued by the kore.ai on client app registration.
 botOptions.clientSecret="qzxggsIqvPL5fmPL5MbXJctF4lYkXcaE6/qOdJSw7RU=";// issued by the kore.ai on client app registration.
